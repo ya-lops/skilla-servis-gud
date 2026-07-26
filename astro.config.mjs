12707f67a0fd0bc95ae7f81b6d6f494d645f3f4f
Vitaliy L <lops@list.ru>
2026-07-27 01:49:45 +0300

    test vercel adapter for demo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,13 @@ import node from "@astrojs/node";
 
 import tailwindcss from "@tailwindcss/vite";
 
-// import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  
-  adapter: node({
-    mode: "standalone",
-  }),
+
+  adapter: vercel({}),
 
   compressHTML: false,
 
